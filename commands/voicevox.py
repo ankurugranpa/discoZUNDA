d178b5a3f4a64735d3e03d6160d383a478338a63
ankuru <ankuru.ahahahaha@gmail.com>
2024-05-09 02:24:05 +0900

    update: [uiのbaseクラスの作成]

--- commands/voicevox.py
+++ commands/voicevox.py
@@ -1,5 +1,7 @@
 import io
 from functools import cache
+from typing import Callable
+from abc import ABC, abstractmethod
 
 import discord
 from discord.ext import commands
@@ -8,36 +10,140 @@ from discord import app_commands
 from util import voicevox
 
 
-class SpeakerButton(discord.ui.Button):
-
+class ButtonUiBase(discord.ui.Button, ABC):
     def __init__(self, name):
         super().__init__(label=name, style=discord.ButtonStyle.primary)
         self.name = name
 
+    @abstractmethod
+    def if_name(self, name:str) -> discord.ui.View:
+        pass
+
     async def callback(self, interaction: discord.Interaction):
-        test_list = [1, 2, 3, 4]
-        # await interaction.response.send_message(f"Cicked:{self.name}")
-        await interaction.response.send_message(view=SpeakerButtonView(test_list))
+            await interaction.response.send_message(view=self.if_name(self.name))
 
 
 
+    
 
-class SpeakerButtonView(discord.ui.View):
 
-    def __init__(self, name_list:list):
+# class ButtonBase(discord.ui.Button):
+# 
+#     def __init__(self, name):
+#         super().__init__(label=name, style=discord.ButtonStyle.primary)
+#         self.name = name
+#         # self.callback_style = "text"
+#         self.callback_style = "ui"
+# 
+#     def set_callback_type(self, type:str):
+#         if type == "text":
+#             self.callback_style = "text"
+#         elif type == "ui":
+#             self.callback_style = "ui"
+# 
+#     def if_name(self, name:str) -> discord.ui.View:
+#         if name == "四国めたん":
+#             test_list = [name, name, name, name]
+#             return ButtonBaseView(test_list)
+#         else:
+#             test_list = ["ちがう", "ちがう"]
+#             return ButtonBaseView(test_list)
+# 
+# 
+#     async def callback(self, interaction: discord.Interaction):
+#         test_list = [1, 2, 3, 4]
+# 
+#         # button_func(self.name)
+#         if self.callback_style == "ui":
+#             await interaction.response.send_message(view=self.if_name(self.name))
+#         elif self.callback_style == "text":
+#             await interaction.response.send_message(f"Cicked:{self.name}")
+#         # await interaction.response.send_message(view=ButtonBaseView(test_list))
+#         # await interaction.response.send_message(view=self.callback_view())
+#         # print(self.name)
+#         return self.name
+
+
+
+# class ButtonBaseView(discord.ui.View):
+# 
+#     def __init__(self, name_list:list):
+#         super().__init__()
+#         self.name_list = name_list
+#         self.name_list_len = len(self.name_list)
+#         self.page_num_f = 1 # 現在のページ
+#         self.page_sum = self._clacpage() # 合計ページ
+#         self.menu()
+#         self._next_page = "NextPage"
+#         self._prev_page = "PrevPage"
+# 
+#     @cache
+#     def _clacpage(self) -> int:
+#         if self.name_list_len < 25:
+#             return 1
+#         elif (self.name_list_len >= 25 and self.name_list_len < 48):
+#             return 2
+#         else:
+#             quotient = (self.name_list_len - 24) // 23
+#             remainde = (self.name_list_len - 24) % 23
+#             return 1 + quotient + remainde
+# 
+# 
+# 
+#     def menu(self):
+# 
+#         # 総ページ数が1
+#         if self.page_sum == 1:
+#             for name in self.name_list:
+#                 self.add_item(TestButtonUiBase(f"{name}"))
+#             
+#         # 総ページ数が2
+#         elif self.page_sum == 2:
+#             if self.page_num_f == 1:
+#                 for name in self.name_list[:24]:
+#                     self.add_item(TestButtonUiBase(f"{name}"))
+#                 self.add_item(TestButtonUiBase(f"{self._next_page}"))
+#                 self.page_num_f = 2
+#             else:
+#                 self.add_item(TestButtonUiBase(f"{self._prev_page}"))
+#                 for name in self.name_list[25:]:
+#                     self.add_item(TestButtonUiBase(f"{name}"))
+# 
+#         # 総ページ数が3~
+#         else:
+#             if self.page_num_f == 1:
+#                 for name in self.name_list[:24]:
+#                     self.add_item(TestButtonUiBase(f"{name}"))
+#                 self.add_item(TestButtonUiBase(f"{self._next_page}"))
+#                 self.page_num_f = 2
+# 
+#             elif self.page_num_f != self.page_sum:
+#                 buf_start = ((self.page_num_f - 2) * 23) + 24 
+#                 buf_end = buf_start + 24
+#                 self.add_item(TestButtonUiBase(f"{self._prev_page}"))
+#                 for name in self.name_list[buf_start:buf_end]:
+#                     self.add_item(TestButtonUiBase(f"{name}"))
+#                 self.add_item(TestButtonUiBase(f"{self._next_page}"))
+#                 self.page_num_f += 1
+# 
+#             elif self.page_num_f == self.page_sum:
+#                 buf_start = ((self.page_num_f - 2) * 23) + 24 
+#                 self.add_item(TestButtonUiBase(f"{self._prev_page}"))
+#                 for name in self.name_list[buf_start:]:
+#                     self.add_item(TestButtonUiBase(f"{name}"))
+
+class ButtonBaseUiView(discord.ui.View, ABC):
+    def __init__(self, ButtonBase:Callable[[str], discord.ui.Button], name_list:list):
         super().__init__()
         self.name_list = name_list
         self.name_list_len = len(self.name_list)
         self.page_num_f = 1 # 現在のページ
         self.page_sum = self._clacpage() # 合計ページ
+        self.ButtonUiBase = ButtonBase
+        self._next_page = "NextPage"
+        self._prev_page = "PrevPage"
         self.menu()
-        # # viewにセレクトを追加
-        # i = 0
-        # for name in name_list:
-        #     if i==25:
-        #         return
-        #     self.add_item(SpeakerButton(f"{name}"))
-        #     i = i+ 1
+        
 
     @cache
     def _clacpage(self) -> int:
@@ -51,49 +157,146 @@ class SpeakerButtonView(discord.ui.View):
             return 1 + quotient + remainde
 
 
-
     def menu(self):
 
         # 総ページ数が1
         if self.page_sum == 1:
             for name in self.name_list:
-                self.add_item(SpeakerButton(f"{name}"))
+                self.add_item(self.ButtonUiBase(name))
+            
         # 総ページ数が2
         elif self.page_sum == 2:
             if self.page_num_f == 1:
                 for name in self.name_list[:24]:
-                    self.add_item(SpeakerButton(f"{name}"))
-                self.add_item(SpeakerButton("NextPage"))
+                    self.add_item(self.ButtonUiBase(name))
+                    # self.add_item(ButtonBase(f"{name}"))
+                self.add_item(self.ButtonUiBase(self._next_page))
                 self.page_num_f = 2
             else:
-                self.add_item(SpeakerButton("PrevPage"))
+                self.add_item(ButtonBase("PrevPage"))
                 for name in self.name_list[25:]:
-                    self.add_item(SpeakerButton(f"{name}"))
+                    self.add_item(self.ButtonUiBase(name))
+                    # self.add_item(ButtonBase(f"{name}"))
 
         # 総ページ数が3~
         else:
             if self.page_num_f == 1:
                 for name in self.name_list[:24]:
-                    self.add_item(SpeakerButton(f"{name}"))
-                self.add_item(SpeakerButton("NextPage"))
+                    self.add_item(self.ButtonUiBase(name))
+                    # self.add_item(ButtonBase(f"{name}"))
+                self.add_item(self.ButtonUiBase(self._next_page))
+                # self.add_item(ButtonBase("NextPage"))
                 self.page_num_f = 2
 
             elif self.page_num_f != self.page_sum:
                 buf_start = ((self.page_num_f - 2) * 23) + 24 
                 buf_end = buf_start + 24
-                self.add_item(SpeakerButton("PrevPage"))
+                self.add_item(self.ButtonUiBase(self._prev_page))
+                # self.add_item(ButtonBase("PrevPage"))
                 for name in self.name_list[buf_start:buf_end]:
-                    self.add_item(SpeakerButton(f"{name}"))
-                self.add_item(SpeakerButton("NextPage"))
+                    self.add_item(self.ButtonUiBase(name))
+                    # self.add_item(ButtonBase(f"{name}"))
+                self.add_item(self.ButtonUiBase(self._next_page))
+                # self.add_item(ButtonBase("NextPage"))
                 self.page_num_f += 1
 
             elif self.page_num_f == self.page_sum:
                 buf_start = ((self.page_num_f - 2) * 23) + 24 
-                self.add_item(SpeakerButton("PrevPage"))
+                self.add_item(self.ButtonUiBase(self._prev_page))
+                # self.add_item(ButtonBase("PrevPage"))
                 for name in self.name_list[buf_start:]:
-                    self.add_item(SpeakerButton(f"{name}"))
+                    self.add_item(self.ButtonUiBase(name))
+                    # self.add_item(ButtonBase(f"{name}"))
+
+class ButtonBaseView(ButtonBaseUiView):
+    def __init__(self, ButtonBase: Callable[[str], discord.ui.Button], name_list: list):
+        super().__init__(ButtonBase, name_list)
+
+class ButtonBase(ButtonUiBase):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def if_name(self, name: str) -> discord.ui.View:
+        if name == "四国めたん":
+            test_list = [name, name, name, name]
+            return ButtonBaseView(ButtonBase, test_list)
+        else:
+            test_list = ["yes", "ちがう"]
+            return ButtonBaseView(ButtonBase, test_list)
 
 
+# class ButtonBaseView(discord.ui.View):
+# 
+#     def __init__(self, name_list:list):
+#         super().__init__()
+#         self.name_list = name_list
+#         self.name_list_len = len(self.name_list)
+#         self.page_num_f = 1 # 現在のページ
+#         self.page_sum = self._clacpage() # 合計ページ
+#         self.menu()
+#         # # viewにセレクトを追加
+#         # i = 0
+#         # for name in name_list:
+#         #     if i==25:
+#         #         return
+#         #     self.add_item(ButtonBase(f"{name}"))
+#         #     i = i+ 1
+# 
+#     @cache
+#     def _clacpage(self) -> int:
+#         if self.name_list_len < 25:
+#             return 1
+#         elif (self.name_list_len >= 25 and self.name_list_len < 48):
+#             return 2
+#         else:
+#             quotient = (self.name_list_len - 24) // 23
+#             remainde = (self.name_list_len - 24) % 23
+#             return 1 + quotient + remainde
+# 
+# 
+# 
+#     def menu(self):
+# 
+#         # 総ページ数が1
+#         if self.page_sum == 1:
+#             for name in self.name_list:
+#                 self.add_item(ButtonBase(f"{name}"))
+#             
+#         # 総ページ数が2
+#         elif self.page_sum == 2:
+#             if self.page_num_f == 1:
+#                 for name in self.name_list[:24]:
+#                     self.add_item(ButtonBase(f"{name}"))
+#                 self.add_item(ButtonBase("NextPage"))
+#                 self.page_num_f = 2
+#             else:
+#                 self.add_item(ButtonBase("PrevPage"))
+#                 for name in self.name_list[25:]:
+#                     self.add_item(ButtonBase(f"{name}"))
+# 
+#         # 総ページ数が3~
+#         else:
+#             if self.page_num_f == 1:
+#                 for name in self.name_list[:24]:
+#                     self.add_item(ButtonBase(f"{name}"))
+#                 self.add_item(ButtonBase("NextPage"))
+#                 self.page_num_f = 2
+# 
+#             elif self.page_num_f != self.page_sum:
+#                 buf_start = ((self.page_num_f - 2) * 23) + 24 
+#                 buf_end = buf_start + 24
+#                 self.add_item(ButtonBase("PrevPage"))
+#                 for name in self.name_list[buf_start:buf_end]:
+#                     self.add_item(ButtonBase(f"{name}"))
+#                 self.add_item(ButtonBase("NextPage"))
+#                 self.page_num_f += 1
+# 
+#             elif self.page_num_f == self.page_sum:
+#                 buf_start = ((self.page_num_f - 2) * 23) + 24 
+#                 self.add_item(ButtonBase("PrevPage"))
+#                 for name in self.name_list[buf_start:]:
+#                     self.add_item(ButtonBase(f"{name}"))
+# 
     # def __top_menu(self) -> list:
     #     """
     #      0 < self.name_list < 25
@@ -101,8 +304,8 @@ class SpeakerButtonView(discord.ui.View):
     #     i = 0
     #     for name in self.name_list:
     #         if i == 25:
-    #             self.add_item(SpeakerButton("NextPage⇒"))
-    #         self.add_item(SpeakerButton(f"{name}"))
+    #             self.add_item(ButtonBase("NextPage⇒"))
+    #         self.add_item(ButtonBase(f"{name}"))
     #     next_list = self.name_list[25:]
     #     return next_list
 
@@ -114,10 +317,10 @@ class SpeakerButtonView(discord.ui.View):
     #     i = 0
     #     for name in name_list:
     #         if i == 0:
-    #             self.add_item(SpeakerButton("⇐PrePage"))
+    #             self.add_item(ButtonBase("⇐PrePage"))
     #         if i == 25:
-    #             self.add_item(SpeakerButton("NextPage⇒"))
-    #         self.add_item(SpeakerButton(f"{name}"))
+    #             self.add_item(ButtonBase("NextPage⇒"))
+    #         self.add_item(ButtonBase(f"{name}"))
     #     next_middle_list = self.name_list[24:]
     #     return next_middle_list
 
@@ -125,8 +328,8 @@ class SpeakerButtonView(discord.ui.View):
     #     i = 0
     #     for name in name_list:
     #         if i == 0:
-    #             self.add_item(SpeakerButton("⇐PrePage"))
-    #         self.add_item(SpeakerButton(f"{name}"))
+    #             self.add_item(ButtonBase("⇐PrePage"))
+    #         self.add_item(ButtonBase(f"{name}"))
 
 
 
@@ -221,7 +424,9 @@ class VoiceVox(commands.Cog):
         #     # view = View()
         #     view.add_item(button)
         #     # view.add_item(discord.ui.Button(label="Click Me!", style=discord.ButtonStyle.green))
-        await interaction.response.send_message(view=SpeakerButtonView(self.voice_vox.speaker_list()))
+        view = ButtonBaseView(ButtonBase, self.voice_vox.speaker_list())
+        # print(type(view))
+        await interaction.response.send_message(view=view)
 
 
 
